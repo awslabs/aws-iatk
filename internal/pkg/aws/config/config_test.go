314024594c67f9749f2d5757cbe499e519262b59
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2023-09-12 14:20:34 -0700

    update user agent

--- internal/pkg/aws/config/config_test.go
+++ internal/pkg/aws/config/config_test.go
@@ -5,14 +5,19 @@ package config
 
 import (
 	"context"
+	"reflect"
+	"runtime"
 	"testing"
+	"zion/internal/pkg/jsonrpc"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	awsmiddleware "github.com/aws/aws-sdk-go-v2/aws/middleware"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestConfigRegion(t *testing.T) {
-	cfg, err := GetAWSConfig(context.TODO(), "us-west-2", "")
+	cfg, err := GetAWSConfig(context.TODO(), "us-west-2", "", nil)
 
 	if err != nil {
 		t.Fail()
@@ -22,11 +27,26 @@ func TestConfigRegion(t *testing.T) {
 }
 
 func TestConfigClientLogMode(t *testing.T) {
-	cfg, err := GetAWSConfig(context.TODO(), "us-west-2", "")
+	cfg, err := GetAWSConfig(context.TODO(), "us-west-2", "", nil)
 
 	if err != nil {
 		t.Fail()
 	}
 
 	assert.Equal(t, aws.LogRetries|aws.LogRequest|aws.LogResponse|aws.LogRequest, cfg.ClientLogMode)
+}
+
+func TestConfigClientUserAgentIsAdded(t *testing.T) {
+	metadata := &jsonrpc.Metadata{
+		Client:  "python",
+		Version: "0.0.3",
+		Caller:  "wait_until_event_matched",
+	}
+	cfg, err := GetAWSConfig(context.TODO(), "us-west-2", "", metadata)
+	require.NoError(t, err)
+	assert.Len(t, cfg.APIOptions, 1)
+	expectFunc := awsmiddleware.AddUserAgentKeyValue("does not matter", "does not matter")
+	expectFuncName := runtime.FuncForPC(reflect.ValueOf(expectFunc).Pointer()).Name()
+	actualFuncName := runtime.FuncForPC(reflect.ValueOf(cfg.APIOptions[0]).Pointer()).Name()
+	assert.Equal(t, actualFuncName, expectFuncName)
 }
--- internal/pkg/jsonrpc/jsonrpc.go
+++ internal/pkg/jsonrpc/jsonrpc.go
@@ -3,6 +3,11 @@ package jsonrpc
 import (
 	"bytes"
 	"encoding/json"
+	"log"
+	"regexp"
+	"strings"
+
+	"golang.org/x/exp/slices"
 )
 
 type Request struct {
@@ -16,13 +21,37 @@ type Request struct {
 	Params json.RawMessage `json:"params"`
 
 	// Metadata contains information about the client
-	Metadata Metadata `json:"metadata"`
+	Metadata *Metadata `json:"metadata"`
 }
 
 type Metadata struct {
 	Client  string `json:"client"`
 	Version string `json:"version"`
 	Caller  string `json:"caller"`
+}
+
+// Returns a string that serves as a user agent key, indicating the client version and the caller method, e.g. 0.0.3#retry_get_trace_tree_until
+func (m *Metadata) UserAgentValue() string {
+	supportedClients := []string{"python"}
+	if !slices.Contains(supportedClients, strings.ToLower(m.Client)) {
+		log.Printf("unregonized client: %v", strings.ToLower(m.Client))
+		return "unknown"
+	}
+
+	// NOTE: pattern from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+	match, err := regexp.Match(`^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$`, []byte(m.Version))
+	if err != nil || !match {
+		log.Printf("invalid version: %v", m.Version)
+		return "unknown"
+	}
+
+	// NOTE: limit caller to has max length of 100 characters, and limit to alphabetical characters and . and _ only
+	match, err = regexp.Match(`^[a-zA-Z_][a-zA-Z0-9_.]{1,99}$`, []byte(m.Caller))
+	if err != nil || !match {
+		log.Printf("invalid caller: %v", m.Caller)
+		return "unknown"
+	}
+	return strings.ToLower(m.Client) + "#" + m.Version + "#" + m.Caller
 }
 
 type Response struct {
