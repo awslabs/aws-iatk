58bdb2fc0354043cbf14312020f0237cb54bf164
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2023-09-28 14:55:17 -0700

    update example02

---
+++ examples/example02-04/tests/python/test_example_02.py
@@ -0,0 +1,97 @@
+import logging
+import json
+import pathlib
+from unittest import TestCase
+
+import requests
+import zion
+
+
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
+
+def read_cdk_outputs() -> dict:
+    with open(pathlib.Path(__file__).parent.parent.parent / "outputs.json") as f:
+        outputs = json.load(f)
+    return outputs
+
+class Example02(TestCase):
+    stack_name: str = "example-stack-01"
+    stack_outputs: dict = read_cdk_outputs().get(stack_name, {}) 
+    z: zion.Zion = zion.Zion()
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.event_bus_name = cls.stack_outputs["EventBusName"]
+        cls.api_endpoint = cls.stack_outputs["ApiEndpoint"]
+        cls.rule_name = cls.stack_outputs["RuleName"].split("|")[1]
+        cls.target_id = cls.stack_outputs["TargetId"]
+
+        # remote orphaned listeners from previous test runs (if any)
+        cls.z.remove_listeners(
+            zion.RemoveListenersParams(
+                tag_filters=[
+                    zion.RemoveListeners_TagFilter(
+                        key="stage",
+                        values=["example02"],
+                    )
+                ]
+            )
+        )
+
+        # create listener
+        listener_id = cls.z.add_listener(
+            zion.AddEbListenerParams(
+                event_bus_name=cls.event_bus_name,
+                rule_name=cls.rule_name,
+                target_id=cls.target_id,
+                tags={"stage": "example02"},
+            )
+        ).id
+        cls.listeners = [listener_id]
+        LOG.debug("created listeners: %s", cls.listeners)
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.z.remove_listeners(
+            zion.RemoveListenersParams(
+                ids=cls.listeners,
+            )
+        )
+        LOG.debug("destroyed listeners: %s", cls.listeners)
+        super().tearDownClass()
+            
+    def test_event_lands_at_eb(self):
+        customer_id = "abc123"
+        requests.post(self.api_endpoint, params={"customerId": customer_id})
+
+        def match_fn(received: str) -> bool:
+            received = json.loads(received)
+            LOG.debug("received: %s", received)
+            return received == customer_id
+
+        self.assertTrue(
+            self.z.wait_until_event_matched(
+                zion.WaitUntilEventMatchedParams(
+                    listener_id=self.listeners[0],
+                    condition=match_fn,
+                )
+            )
+        )
+
+    def test_poll_events(self):
+        customer_id = "def456"
+        requests.post(self.api_endpoint, params={"customerId": customer_id})
+
+        received = self.z.poll_events(
+            zion.PollEventsParams(
+                listener_id=self.listeners[0],
+                wait_time_seconds=5,
+                max_number_of_messages=10,
+            )
+        ).events
+        LOG.debug("received: %s", received)
+        self.assertGreaterEqual(len(received), 1)
+        self.assertEqual(json.loads(received[0]), customer_id)
