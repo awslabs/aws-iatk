55db86f4fe47b56a1afd67c1ec8a44bf38a1d7cc
James Sheng <jysheng@amazon.com>
2023-10-03 14:48:13 -0700

    build-linux

--- python-client/build-linux.sh
+++ python-client/build-linux.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/bash
-archs=(amd64 arm64 ppc64le ppc64 s390x)
 pip install build
-for arch in ${archs[@]}
-do  
-    echo ${arch}
-	export GOARCH=${arch}
-    python3 -m build -w
-done
+export GOARCH=${GO_ARCH}
+python3 -m build -w
