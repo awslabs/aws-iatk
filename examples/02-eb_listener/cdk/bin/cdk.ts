208a283e9f06f545d3c70fdf0cc473d302a40e3f
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2023-10-18 13:20:16 -0700

    remove examples (moved to its own branch) (#66)

--- examples/02-eb_listener/cdk/bin/cdk.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { EbStack } from '../lib/eb-stack';
-
-const namespace = process.env.NAMESPACE || 'cdk-example';
-
-const app = new cdk.App();
-const ebStack = new EbStack(app, `${namespace}-ebStack`);
