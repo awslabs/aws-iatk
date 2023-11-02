f7ab59d33044fd99cc8ca6404eb3af6ebcea163b
sidhujus <105385029+sidhujus@users.noreply.github.com>
2023-11-02 13:11:45 -0700

    change condition to assertion_fn (#82)
    * change condition to assertion_fn
    
    * improve logging
    
    * remove leftover debugging code
    
    * remove except Exception
    
    * update examples
    
    * remove leftover code in example
    
    Co-authored-by: Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Wing Fung Lau <4760060+hawflau@users.noreply.github.com>

--- python-client/tests/integration/test_harness/eventbridge/poll_events/test_wait_until_event_matched.py
+++ python-client/tests/integration/test_harness/eventbridge/poll_events/test_wait_until_event_matched.py
@@ -39,39 +39,32 @@ class EbEvent:
         }
 
 
-def condition_func_0(received: str) -> bool:
+def assertion_func_0(received: str) -> None:
     LOG.debug("received: %s", received)
     payload = json.loads(received)
-    try:
-        detail = payload["detail"]
-        detail_type = payload["detail-type"]
-        source = payload["source"]
-        matched = (
-            detail["abc"] == "def"
-            and detail["id"] == "2"
-            and source == "com.test.0"
-            and detail_type == "foo"
-        )
-        LOG.debug("matched: %s", matched)
-        return matched
-    except Exception as e:
-        LOG.debug("error: %s", e)
-        return False
+    detail = payload["detail"]
+    detail_type = payload["detail-type"]
+    source = payload["source"]
+    assert detail["abc"] == "def"
+    assert detail["id"] == "2"
+    assert source == "com.test.0"
+    assert detail_type == "foo"
+    LOG.debug("matched: %s", True)
 
 
-def condition_func_1(received: str) -> bool:
+def assertion_func_1(received: str) -> None:
     LOG.debug("received: %s", received)
-    return received == '"hello, world!"'
+    assert received == '"hello, world!"'
 
 
-def condition_func_2(received: str) -> bool:
+def assertion_func_2(received: str) -> None:
     LOG.debug("received: %s", received)
-    return received == '"xyz"'
+    assert received == '"xyz"'
 
 
-def condition_func_3(received: str) -> bool:
+def assertion_func_3(received: str) -> None:
     LOG.debug("received: %s", received)
-    return received == '{"source": "com.test.3", "foo": "bar"}'
+    assert received == '{"source": "com.test.3", "foo": "bar"}'
 
 @dataclass
 class EbConfiguration:
@@ -219,7 +212,7 @@ class TestCTK_wait_until_event_matched(TestCase):
                         detail={"id": "0", "abc": "def"},
                     ),
                 ],
-                condition_func_0,
+                assertion_func_0,
                 10,
             ),
             (
@@ -251,7 +244,7 @@ class TestCTK_wait_until_event_matched(TestCase):
                         detail={"id": "0", "abc": "def"},
                     ),
                 ],
-                condition_func_1,
+                assertion_func_1,
                 5,
             ),
             (
@@ -283,7 +276,7 @@ class TestCTK_wait_until_event_matched(TestCase):
                         detail={"id": "2", "abc": "def"},
                     ),
                 ],
-                condition_func_2,
+                assertion_func_2,
                 5,
             ),
             (
@@ -315,7 +308,7 @@ class TestCTK_wait_until_event_matched(TestCase):
                         detail={"id": "0", "abc": "def", "foo": "bar"},
                     ),
                 ],
-                condition_func_3,
+                assertion_func_3,
                 5,
             ),
         ]
@@ -324,7 +317,7 @@ class TestCTK_wait_until_event_matched(TestCase):
         self,
         listener_idx: int,
         events: List[EbEvent],
-        condition_func: Callable[[str], bool],
+        assertion_func: Callable[[str], bool],
         timeout_seconds: int,
     ):
         LOG.debug("purging listener to delete events from previous tests")
@@ -336,7 +329,7 @@ class TestCTK_wait_until_event_matched(TestCase):
         LOG.debug("waiting for event")
         found = self.ctk.wait_until_event_matched(
             listener_id=self.listener_ids[listener_idx],
-            condition=condition_func,
+            assertion_fn=assertion_func,
             timeout_seconds=timeout_seconds
         )
         self.assertTrue(found)
@@ -346,7 +339,7 @@ class TestCTK_wait_until_event_matched(TestCase):
             (  # no event is sent
                 0,
                 [],
-                condition_func_0,
+                assertion_func_0,
                 10,
             ),
             (  # all events don't match
@@ -373,7 +366,7 @@ class TestCTK_wait_until_event_matched(TestCase):
                         detail={"id": "0", "abc": "def"},
                     ),
                 ],
-                condition_func_2,
+                assertion_func_2,
                 5,
             ),
         ]
@@ -382,7 +375,7 @@ class TestCTK_wait_until_event_matched(TestCase):
         self,
         listener_idx: int,
         events: List[EbEvent],
-        condition_func: Callable[[str], bool],
+        assertion_func: Callable[[str], bool],
         timeout_seconds: int,
     ):
         LOG.debug("purging listener to delete events from previous tests")
@@ -394,7 +387,7 @@ class TestCTK_wait_until_event_matched(TestCase):
         LOG.debug("waiting for event")
         found = self.ctk.wait_until_event_matched(
             listener_id=self.listener_ids[listener_idx],
-            condition=condition_func,
+            assertion_fn=assertion_func,
             timeout_seconds=timeout_seconds
         )
         self.assertFalse(found)
@@ -404,7 +397,7 @@ class TestCTK_wait_until_event_matched(TestCase):
         with self.assertRaises(InvalidParamException):
             self.ctk.wait_until_event_matched(
                 listener_id=self.listener_ids[listener_idx],
-                condition=condition_func_0,
+                assertion_fn=assertion_func_0,
                 timeout_seconds=10000
             )
 
--- python-client/tests/unit/test_retry_until.py
+++ python-client/tests/unit/test_retry_until.py
@@ -19,8 +19,8 @@ class TestCTK_retry_until_timeout(TestCase):
 
     def test_retry_timeout_is_default_fail(self):
         def num_is_ten(val):
-            return val == 10
-        @self.ctk.retry_until(condition=num_is_ten)
+            assert val == 10
+        @self.ctk.retry_until(assertion_fn=num_is_ten)
         def num_add_one_slow():
             self.num = self.num + 1
             return self.num
@@ -33,8 +33,8 @@ class TestCTK_retry_until_timeout(TestCase):
 
     def test_retry_timeout_is_default_pass(self):
         def num_is_five(val):
-            return val == 5
-        @self.ctk.retry_until(condition=num_is_five)
+            assert val == 5
+        @self.ctk.retry_until(assertion_fn=num_is_five)
         def num_add_one():
             self.num = self.num + 1
             return self.num
@@ -48,8 +48,8 @@ class TestCTK_retry_until_timeout(TestCase):
     @pytest.mark.timeout(timeout=2500, method="thread")
     def test_retry_timeout_is_infinite(self):
         def num_is_ten(val):
-            return val == 10
-        @self.ctk.retry_until(condition=num_is_ten, timeout=0)
+            assert val == 10
+        @self.ctk.retry_until(assertion_fn=num_is_ten, timeout=0)
         def num_add_one_slow():
             time.sleep(2)
             self.num = self.num + 1
@@ -63,7 +63,7 @@ class TestCTK_retry_until_timeout(TestCase):
 
     def test_retry_condition_not_function_error(self):
         with pytest.raises(TypeError) as e:  
-            @self.ctk.retry_until(condition=0, timeout=5)
+            @self.ctk.retry_until(assertion_fn=0, timeout=5)
             def num_add_one():
                 self.num = self.num + 1
                 return self.num 
@@ -72,9 +72,9 @@ class TestCTK_retry_until_timeout(TestCase):
     
     def test_retry_timeout_wrong_type_error(self):
         def num_is_ten(val):
-            return val == 10
+            assert val == 10
         with pytest.raises(TypeError) as e:  
-            @self.ctk.retry_until(condition=num_is_ten, timeout="test")
+            @self.ctk.retry_until(assertion_fn=num_is_ten, timeout="test")
             def num_add_one():
                 self.num = self.num + 1
                 return self.num 
@@ -83,9 +83,9 @@ class TestCTK_retry_until_timeout(TestCase):
 
     def test_retry_timeout_less_than_zero_error(self):
         def num_is_ten(val):
-            return val == 10
+            assert val == 10
         with pytest.raises(ValueError) as e:  
-            @self.ctk.retry_until(condition=num_is_ten, timeout=-1)
+            @self.ctk.retry_until(assertion_fn=num_is_ten, timeout=-1)
             def num_add_one():
                 self.num = self.num + 1
                 return self.num 
@@ -94,8 +94,8 @@ class TestCTK_retry_until_timeout(TestCase):
 
     def test_retry_should_pass(self):
         def num_is_five(val):
-            return val == 5
-        @self.ctk.retry_until(condition=num_is_five, timeout=5)
+            assert val == 5
+        @self.ctk.retry_until(assertion_fn=num_is_five, timeout=5)
         def num_add_one():
             self.num = self.num + 1
             return self.num
@@ -106,8 +106,8 @@ class TestCTK_retry_until_timeout(TestCase):
     def test_retry_should_timeout(self):
         start = time.time()
         def num_is_negative(val):
-            return val < 0
-        @self.ctk.retry_until(condition=num_is_negative, timeout=5)
+            assert val < 0
+        @self.ctk.retry_until(assertion_fn=num_is_negative, timeout=5)
         def num_add_one():
             self.num = self.num + 1
             return self.num
@@ -119,8 +119,8 @@ class TestCTK_retry_until_timeout(TestCase):
 
     def test_retry_should_pass_multiple_arguments(self):
         def num_is_five(val):
-            return val == 5
-        @self.ctk.retry_until(condition=num_is_five, timeout=5)
+            assert val == 5
+        @self.ctk.retry_until(assertion_fn=num_is_five, timeout=5)
         def num_add_one(dummy, dummy1, dummy2="dummy2"):
             self.num = self.num + 1
             return self.num
@@ -131,8 +131,8 @@ class TestCTK_retry_until_timeout(TestCase):
     def test_retry_should_timeout_multiple_arguments(self):
         start = time.time()
         def num_is_negative(val):
-            return val < 0
-        @self.ctk.retry_until(condition=num_is_negative, timeout=5)
+            assert val < 0
+        @self.ctk.retry_until(assertion_fn=num_is_negative, timeout=5)
         def num_add_one(dummy, dummy1, dummy2="dummy2"):
             self.num = self.num + 1
             return self.num
